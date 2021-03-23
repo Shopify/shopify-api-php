0efcf3c90781b4c87787e918d78cf9fcaf930736
Paulina Khew <paulina.khew@shopify.com>
2021-03-23 17:31:01 -0400

    Update Session class and update tests

--- src/Session.php
+++ src/Session.php
@@ -3,21 +3,47 @@
 declare(strict_types=1);
 
 namespace Shopify;
+use DateTime;
 
 /**
  * Stores App information from logged in merchants so they can make authenticated requests to the Admin API.
  */
 class Session
 {
-    public function __construct(
-        protected string $shop,
-        protected string $state,
-        protected string $scope,
-        protected ?string $expires,
-        protected ?bool $isOnline,
-        protected ?string $accessToken = null,
-    )
+    private string $shop;
+    private string $state;
+    private string $scope;
+    private ?DateTime $expires = null;
+    private ?bool $isOnline = false;
+    private ?string $accessToken = null;
+
+    public function __construct(private string $id)
     {
+    }
+
+    public function getShop()
+    {
+        return $this->shop;
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    public function getScope()
+    {
+        return $this->scope;
+    }
+
+    public function getExpires()
+    {
+        return $this->expires;
+    }
+
+    public function getIsOnline()
+    {
+        return $this->isOnline;
     }
 
     public function getAccessToken()
@@ -25,8 +51,34 @@ class Session
         return $this->accessToken;
     }
 
-    public function getShop()
+    public function setShop(string $shop)
     {
-        return $this->shop;
+        return $this->shop = $shop;
+    }
+
+    public function setState(string $state)
+    {
+        return $this->state = $state;
+    }
+
+    public function setScope(string $scope)
+    {
+        return $this->scope = $scope;
+    }
+
+    public function setExpires(string $expires)
+    {
+        $date = new DateTime($expires);
+        return $this->expires = $date;
+    }
+
+    public function setIsOnline(bool $isOnline)
+    {
+        return $this->isOnline = $isOnline;
+    }
+
+    public function setAccessToken(string $accessToken)
+    {
+        return $this->accessToken = $accessToken;
     }
 }
--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -3,13 +3,24 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use DateTime;
 
 final class SessionTest extends TestCase
 {
     public function testSessionFunctions()
     {
-        $session = new Shopify\Session('my-shop.myshopify.com', '12345');
+        $session = new Shopify\Session('12345');
+        $session->setShop('my-shop.myshopify.com');
+        $session->setState('asdf1234');
+        $session->setScope('read_products');
+        $session->setExpires('January 25, 2021');
+        $session->setIsOnline(true);
+        $session->setAccessToken('24ssdf243u2ohfd21');
         $this->assertEquals('my-shop.myshopify.com', $session->getShop());
-        $this->assertEquals('12345', $session->getAccessToken());
+        $this->assertEquals('asdf1234', $session->getState());
+        $this->assertEquals('read_products', $session->getScope());
+        $this->assertEquals(new DateTime('January 25, 2021'), $session->getExpires());
+        $this->assertEquals(true, $session->getIsOnline());
+        $this->assertEquals('24ssdf243u2ohfd21', $session->getAccessToken());
     }
 }

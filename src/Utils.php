0878d69707d067e34474a7e9832567befbce84e8
Carmela Leung <carmela.leung@shopify.com>
2021-04-16 16:20:35 -0400

    Add loadOfflineSession method

--- src/Utils.php
+++ src/Utils.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Shopify;
 
+use Shopify\Context;
+use Shopify\Auth\Session;
+use Shopify\Auth\OAuth;
 use Shopify\Exception\InvalidArgumentException;
 
 /**
@@ -95,5 +98,29 @@ final class Utils
         $referenceNumeric = (int)str_replace('-', '', $referenceVersion);
 
         return $currentNumeric >= $referenceNumeric;
+    }
+
+    /**
+     * Loads an offline session
+     * No validation is done on the shop param; ensure it comes from a safe source
+     *
+     * @param string $shop           The shop url to find the offline session for
+     * @param bool   $includeExpired Optionally include expired sessions, defaults to false
+     *
+     * @return Session|null If exists, the most recent session
+     * @throws \Shopify\Exception\ContextUninitializedException
+     */
+    public static function loadOfflineSession(string $shop, bool $includeExpired = false): ?Session
+    {
+        Context::throwIfUninitialized();
+
+        $sessionId = OAuth::getOfflineSessionId($shop);
+        $session = Context::$SESSION_STORAGE->loadSession($sessionId);
+
+        if ($session && !$includeExpired && !$session->isValid()) {
+            return null;
+        }
+
+        return $session;
     }
 }

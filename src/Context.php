8862b2568fa86a3de522de3356348ff7ebbcbe4a
Paulo Margarido <paulo.margarido@shopify.com>
2021-04-21 08:58:37 -0400

    Add optional logger attribute to Context

--- src/Context.php
+++ src/Context.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Shopify;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
 use Shopify\Auth\SessionStorage;
 use Shopify\Clients\Curl;
 use Shopify\Auth\Scopes;
@@ -25,20 +27,23 @@ class Context
     public static bool $IS_PRIVATE_APP = false;
     public static ?string $USER_AGENT_PREFIX = null;
     public static int $RETRY_TIME_IN_SECONDS = 1;
+    public static ?LoggerInterface $LOGGER = null;
     private static bool $IS_INITIALIZED = false;
 
     /**
      * Initializes Context object
      *
-     * @param string         $apiKey          App API key
-     * @param string         $apiSecretKey    App API secret
-     * @param string|array   $scopes          App scopes
-     * @param string         $hostName        App host name e.g. www.google.ca
-     * @param SessionStorage $sessionStorage  Session storage strategy
-     * @param string         $apiVersion      App API key, defaults to unstable
-     * @param bool           $isEmbeddedApp   Whether the app is an embedded app, defaults to true
-     * @param bool           $isPrivateApp    Whether the app is a private app, defaults to false
-     * @param string         $userAgentPrefix Prefix for user agent header sent with a request, defaults to empty string
+     * @param string          $apiKey          App API key
+     * @param string          $apiSecretKey    App API secret
+     * @param string|array    $scopes          App scopes
+     * @param string          $hostName        App host name e.g. www.google.ca
+     * @param SessionStorage  $sessionStorage  Session storage strategy
+     * @param string          $apiVersion      App API key, defaults to unstable
+     * @param bool            $isEmbeddedApp   Whether the app is an embedded app, defaults to true
+     * @param bool            $isPrivateApp    Whether the app is a private app, defaults to false
+     * @param string          $userAgentPrefix Prefix for user agent header sent with a request, defaults to empty
+     *                                         string
+     * @param LoggerInterface $logger          App logger, so the library can add its own logs to it
      */
     public static function initialize(
         string $apiKey,
@@ -50,6 +55,7 @@ class Context
         bool $isEmbeddedApp = true,
         bool $isPrivateApp = false,
         string $userAgentPrefix = '',
+        LoggerInterface $logger = null,
     ): void {
         $authScopes = new Scopes($scopes);
 
@@ -84,6 +90,7 @@ class Context
         self::$IS_EMBEDDED_APP = $isEmbeddedApp;
         self::$IS_PRIVATE_APP = $isPrivateApp;
         self::$USER_AGENT_PREFIX = $userAgentPrefix;
+        self::$LOGGER = $logger;
 
         self::$IS_INITIALIZED = true;
     }
@@ -111,5 +118,24 @@ class Context
         if (self::$IS_PRIVATE_APP) {
             throw new PrivateAppException($message);
         }
+    }
+
+    /**
+     * Logs a message using the defined callback. If none is set, the message is ignored.
+     *
+     * @param string $message The message to log
+     * @param string $level   One of the \Psr\Log\LogLevel::* consts, defaults to INFO
+     *
+     * @throws \Shopify\Exception\UninitializedContextException
+     */
+    public static function log(string $message, string $level = LogLevel::INFO): void
+    {
+        self::throwIfUninitialized();
+
+        if (!self::$LOGGER) {
+            return;
+        }
+
+        self::$LOGGER->log($level, $message);
     }
 }

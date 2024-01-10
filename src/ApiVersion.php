02d08a70852c5a9e6e03a73bffa1c2ecdd88f566
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2024-01-10 17:35:45 -0300

    Adding support for 2024-01 API version

--- src/ApiVersion.php
+++ src/ApiVersion.php
@@ -23,7 +23,9 @@ class ApiVersion
     /** @var string */
     public const OCTOBER_2023 = "2023-10";
     /** @var string */
-    public const LATEST = self::OCTOBER_2023;
+    public const JANUARY_2024 = "2024-01";
+    /** @var string */
+    public const LATEST = self::JANUARY_2024;
 
     private static $ALL_VERSIONS = [
         self::UNSTABLE,
@@ -34,6 +36,7 @@ class ApiVersion
         self::APRIL_2023,
         self::JULY_2023,
         self::OCTOBER_2023,
+        self::JANUARY_2024,
     ];
 
     public static function isValid(string $version): bool
